13c6bdc863f30b7a426651b22e34c4a79123610e
Fridon Danelyan <7125622+danelyan@users.noreply.github.com>
2019-02-27 18:24:06 +0300

    Bfoke test (#6)
    * Bfoke test
    
    * Update app/src/test/java/ru/cometrica/demoapp/BusinessLogicTest.kt
    
    Fix
    
    Co-Authored-By: danelyan <7125622+danelyan@users.noreply.github.com>
    
    * Fix

--- app/src/test/java/ru/cometrica/demoapp/BusinessLogicTest.kt
+++ app/src/test/java/ru/cometrica/demoapp/BusinessLogicTest.kt
@@ -1,5 +1,7 @@
 package ru.cometrica.demoapp
 
+import io.reactivex.Maybe
+import io.reactivex.Single
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -9,4 +11,19 @@ class BusinessLogicTest {
     fun plus() {
         assertEquals(BusinessLogic().plus(1, 1), 2)
     }
+
+    @Test
+    fun test() {
+        getValue()
+            .subscribe({ println("$it\n") }, { System.err.println(it) })
+    }
+
+    fun getValueFromCache(): Maybe<String> = Maybe.empty()
+
+    fun getValueFromCloud() = Maybe.empty<String>()
+
+    fun getValue(): Single<String> =
+        getValueFromCache()
+            .switchIfEmpty(getValueFromCloud())
+            .toSingle("")
 }
